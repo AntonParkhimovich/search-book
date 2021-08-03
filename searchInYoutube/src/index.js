eb391731fd62200217032a5734e631f0cc524638
Anton Parkhimovich <pad1907@gmail.com>
2021-08-03 21:59:58 +0300

    feat: add redux-persist support

--- searchInYoutube/src/index.js
+++ searchInYoutube/src/index.js
@@ -6,14 +6,26 @@ import reportWebVitals from './reportWebVitals';
 import {compose, createStore} from 'redux'
 import { rootReducer } from './redux/rootReducer';
 import { Provider } from 'react-redux';
-const store = createStore(rootReducer, compose(
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { PersistGate } from 'redux-persist/integration/react'
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = createStore(persistedReducer, compose(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   ))
+ let persistor = persistStore(store) 
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store ={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <App />
+    </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')

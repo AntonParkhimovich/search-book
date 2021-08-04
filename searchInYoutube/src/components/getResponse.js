ec7649db32581bee2a2885dec9fe01f25a47c429
AntonParkhimovich <pad1907@gmail.com>
2021-08-04 18:09:35 +0000

    refactor: add get request for getting statictics video

--- searchInYoutube/src/components/getResponse.js
+++ searchInYoutube/src/components/getResponse.js
@@ -1,10 +1,25 @@
 import axios from 'axios'
 import lodash from 'lodash'
+let  responseStringId =''
+
  export const getResponce = (video, setStore) =>{
      if (video !== ''){
         axios.get(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyBKFVnu-NyTs5mvtsWq0qN8cwrLI-AOFAA&type=video&part=snippet&maxResults=100&q=${video}`)
-        .then(response =>{return lodash.toArray(response.data.items)})
-        .then(response=> setStore(lodash.chunk(response, 5) ))
+        .then(response => {
+            response.data.items.map((item, index) =>{
+            index===0 ? 
+            responseStringId = `${item.id.videoId}`: 
+            responseStringId = `${responseStringId},${item.id.videoId}`})
+            axios.get(`https://www.googleapis.com/youtube/v3/videos?key=AIzaSyBKFVnu-NyTs5mvtsWq0qN8cwrLI-AOFAA&id=${responseStringId}&part=statistics`)
+            .then(data=>data.data.items.map((item, index) =>{
+               response.data.items[index].statistics = item.statistics
+            }))
+            return response
+        })
+        
+         
+      .then(response=> {setStore(lodash.chunk(response.data.items, 5))})
+         
      }
     
 }

6244e3d1e47f026bf5b106497ac57e5c7a936d2e
Anton Parkhimovich <pad1907@gmail.com>
2021-08-08 01:34:18 +0300

    ауфеЖ фввв ыендуы шсщты

--- searchInYoutube/src/components/videoCard.js
+++ searchInYoutube/src/components/videoCard.js
@@ -3,13 +3,25 @@ import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import ThumbDownIcon from '@material-ui/icons/ThumbDown';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import CommentIcon from '@material-ui/icons/Comment';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import { makeStyles } from '@material-ui/styles';
+import { Link, useRouteMatch } from 'react-router-dom';
+const useStyles = makeStyles({
+    root: {
+     width:'100px',
+     height: '100px'
+    },
+  });
 export const VideoCard=(props)=>{
+    let { path, url } = useRouteMatch()
+    const classes = useStyles();
     let {video}= props
     console.log(video);
     return(
     <div className ={'video-card'}>
         <div className={'video-card__header'}>
-        <iframe width="100%" height="100%" src={`https://www.youtube.com/embed/${video.id.videoId}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture " allowfullscreen='1'></iframe>
+        <Link className ={`link-video`} to={`${url}/${video.id.videoId}`} ><PlayCircleFilledIcon className={classes.root}></PlayCircleFilledIcon></Link>
+        
         </div>
         <div className={'video-card__main'}>
             <h2 className={'video-card__chanell'}>{video.snippet.channelTitle}</h2>
@@ -26,3 +38,5 @@ export const VideoCard=(props)=>{
     </div>
     ) 
 }
+
+{/* <iframe width="100%" height="100%" src={`https://www.youtube.com/embed/${video.id.videoId}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture " allowfullscreen='1'></iframe> */}

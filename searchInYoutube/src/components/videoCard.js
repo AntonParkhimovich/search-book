d6df8aabbee213ac967187f7cccd6b6129a788bd
Anton Parkhimovich <pad1907@gmail.com>
2021-08-07 17:18:12 +0300

    feat: add react animations and material-ui icons

--- searchInYoutube/src/components/videoCard.js
+++ searchInYoutube/src/components/videoCard.js
@@ -1,4 +1,8 @@
 import '../styles/card.scss'
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ThumbDownIcon from '@material-ui/icons/ThumbDown';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import CommentIcon from '@material-ui/icons/Comment';
 export const VideoCard=(props)=>{
     let {video}= props
     console.log(video);
@@ -8,13 +12,13 @@ export const VideoCard=(props)=>{
         <iframe width="100%" height="100%" src={`https://www.youtube.com/embed/${video.id.videoId}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture " allowfullscreen='1'></iframe>
         </div>
         <div className={'video-card__main'}>
-            <h2 className={'video-card__title'}>{}</h2>
+            <h2 className={'video-card__chanell'}>{video.snippet.channelTitle}</h2>
+            <h2 className={'video-card__title'}>{video.snippet.title}</h2>
             <ul className={'statistics'}>
-                <li className={'statistics-items'}></li>
-                <li className={'statistics-items'}></li>
-                <li className={'statistics-items'}></li>
-                <li className={'statistics-items'}></li>
-                <li className={'statistics-items'}></li>
+                <li className={'statistics-items'}><ThumbUpIcon/>{video.statistics.likeCount}</li>
+                <li className={'statistics-items'}><ThumbDownIcon/>{video.statistics.dislikeCount}</li>
+                <li className={'statistics-items'}><VisibilityIcon/>{video.statistics.viewCount}</li>
+                <li className={'statistics-items'}><CommentIcon/>{video.statistics.commentCount}</li>
             </ul>
         </div>
         <div className={'video-card___footer'}>

5ebe55d6ef57531df0fe1fbb68454bcbae84808f
Anton Parkhimovich <pad1907@gmail.com>
2021-08-08 01:35:27 +0300

    feat:add animation

--- searchInYoutube/src/components/MainPaige.js
+++ searchInYoutube/src/components/MainPaige.js
@@ -1,10 +1,18 @@
  import { VideoCard } from "./VideoCard";
+ import styled, { keyframes } from 'styled-components';
+ import { fadeIn } from 'react-animations'
+const FadeIn = styled.div`animation: 1s ${keyframes`${fadeIn}`}`;
  export const MainPaige = (props)=>{
      const {videos} = props
     return(
         <>
        {videos.map((item, index)=>{
-           return <VideoCard video={item} key={index}/>
+           return(
+        <FadeIn>
+               <VideoCard video={item} key={index}/>
+        </FadeIn>    
+           ) 
+           
        })}
        </>
     )

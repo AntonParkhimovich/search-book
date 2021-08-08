4b33f0ea7060a6753e4ab4396b1fca7f0f9d07a7
Anton Parkhimovich <pad1907@gmail.com>
2021-08-08 19:17:35 +0300

    add : button goback

--- searchInYoutube/src/components/Video.js
+++ searchInYoutube/src/components/Video.js
@@ -1,8 +1,10 @@
 import { useEffect } from 'react';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
 import "../styles/video.scss"
+import { useHistory } from 'react-router';
 const Video = (props)=>{
     let {video}=props
+    const history = useHistory()
     useEffect(() => {
         document.body.style.overflow = 'hidden';
       
@@ -10,13 +12,15 @@ const Video = (props)=>{
             document.body.style.overflow = 'auto'
         };
       }, []);
-
+    const clickHandler=()=>{
+        history.goBack()
+    }
     return(
         <div className={'video-wrapper'}>
             <div className ={'video'}>
             <iframe width="100%" height="100%" src={`https://www.youtube.com/embed/${video.id.videoId}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture " allowfullscreen='1'></iframe>
             </div>
-            <ArrowBackIosIcon style={{ color: 'white'}}/>
+            <ArrowBackIosIcon style={{ color: 'white', fontSize: 40, cursor:"pointer"}} onClick ={clickHandler}/>
         </div>
     )
 }
